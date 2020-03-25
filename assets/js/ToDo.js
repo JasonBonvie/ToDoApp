6f057ab661ea012a761e0f8c2dd70178128e3fc1
Jason Bonvie <jasonb3194@gmail.com>
2020-03-25 01:12:51 -0400

    base function of To-Do list

--- assets/js/ToDo.js
+++ assets/js/ToDo.js
@@ -1,13 +1,32 @@
 // check of specific todo's by clicking 
-
 $("li").click(function(){
-	$(this).css({
-		color: "gray";
-		textDecoration: "line-through";
-	})
 
-	
+	// =====Boolean logic way to toggle click output ========
+	// 	if($(this).css("color") === "rgb(128, 128, 128)"){
+	// 	$(this).css({
+	// 		color: "black",
+	// 		textDecoration: "none"
+	// 	});
+	// }
+	// else{
+	// 	$(this).css({
+	// 		color: "gray",
+	// 		textDecoration: "line-through"
+	// 	});
+	// }
+	$(this).toggleClass("compleated");
 	//or
 	//$(this).css("color", "gray")
 	//$(this).css("text-decoration", "line-through")
-})
+});
+
+// Click on X to delete toDo
+
+$("span").click(function(event){
+
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+
+});
